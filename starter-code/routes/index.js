41a5e03b121124cdce2ad6e6b9cdfd2025f286f1
Bianca Stöckl <bianca@artofbibi.de>
2019-07-03 15:44:50 +0200

    initial

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+const loginCheck = () => {
+  return (req, res, next) => {
+    if (req.isAuthenticated()) next();
+    else res.redirect("/login");
+  };
+};
+
+router.get("/secret", loginCheck(), (req, res) => {
+  console.log("secret: ", req.session);
+  res.render("secret");
 });
 
 module.exports = router;
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,75 @@
-const express        = require("express");
+const express = require("express");
+const bcrypt = require("bcrypt");
 const passportRouter = express.Router();
-// Require user model
-
-// Add bcrypt to encrypt passwords
-
-// Add passport 
-
-
+const User = require("../models/user");
+const passport = require("passport");
 const ensureLogin = require("connect-ensure-login");
 
+passportRouter.get("/signup", (req, res) => {
+  res.render("passport/signup");
+});
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+passportRouter.get("/login", (req, res) => {
+  res.render("passport/login", { errorMessage: req.flash("error") });
+});
+
+passportRouter.get(
+  "/private-page",
+  ensureLogin.ensureLoggedIn(),
+  (req, res) => {
+    res.render("passport/private", { user: req.user });
+  }
+);
+
+passportRouter.post(
+  "/passport/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/passport/login",
+    failureFlash: true
+  })
+);
+
+passportRouter.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!password || !username) {
+    res.render("passport/signup", { errorMessage: "Both fields are required" });
+
+    return;
+  } else if (password.length < 8) {
+    res.render("passport/signup", {
+      errorMessage: "Password needs to be 8 characters min"
+    });
+
+    return;
+  }
+  User.findOne({ username: username })
+    .then(user => {
+      if (user) {
+        res.render("passport/signup", {
+          errorMessage: "This username is already taken"
+        });
+
+        return;
+      }
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(password, salt);
+
+      return User.create({
+        username,
+        password: hash
+      }).then(data => {
+        res.redirect("/");
+      });
+    })
+    .catch(err => {
+      res.render("passport/signup", { errorMessage: err._message });
+    });
+});
+passportRouter.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = passportRouter;
